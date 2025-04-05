8aba72946a6b6224ffe73ec5e5a76ed43def9725
R-andomUserX <enriquez.mikhaela@ess.edu.ph>
2025-04-05 23:41:05 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,144 @@
+input.onButtonPressed(Button.A, function () {
+    if (rolled == 0) {
+        Mission = randint(0, 17)
+        if (Mission == 0 && (_0 == 0 && rolled == 0)) {
+            basic.showNumber(0)
+            basic.showString("Today's Mission do the most embarrassing dance in front of the class and walk away like nothing happened.")
+            _0 = 1
+            rolled = 1
+        } else if (Mission == 1 && (_1 == 0 && rolled == 0)) {
+            basic.showNumber(1)
+            basic.showString("Today's Mission pretend to be a cat for the whole day. (Say meow every time someone starts talking to you) ")
+            _1 = 1
+            rolled = 1
+        } else if (Mission == 2 && (_2 == 0 && rolled == 0)) {
+            basic.showNumber(2)
+            basic.showString("Today's Mission be annoying your best friend.")
+            _2 = 1
+            rolled = 1
+        } else if (Mission == 3 && (_3 == 0 && rolled == 0)) {
+            basic.showNumber(3)
+            basic.showString("Today's Mission imitate someone until they guess that you're imitating them.")
+            _3 = 1
+            rolled = 1
+        } else if (Mission == 4 && (_4 == 0 && rolled == 0)) {
+            basic.showNumber(4)
+            basic.showString("Today's Mission sing \"I want it that way\" with the weirdest voice ever")
+            _4 = 1
+            rolled = 1
+        } else if (Mission == 5 && (_5 == 0 && rolled == 0)) {
+            basic.showNumber(5)
+            basic.showString("Today's Mission ask people to call you with the nickname you hate the most")
+            _5 = 1
+            rolled = 1
+        } else if (Mission == 6 && (_6 == 0 && rolled == 0)) {
+            basic.showNumber(6)
+            basic.showString("Today's Mission use an British or Australian Voice for the whole day")
+            _6 = 1
+            rolled = 1
+        } else if (Mission == 7 && (_7 == 0 && rolled == 0)) {
+            basic.showNumber(7)
+            basic.showString("Today's Mission draw your least favorite character in your favorite game and tell everyone its your favorite character")
+            _7 = 1
+            rolled = 1
+        } else if (Mission == 8 && (_8 == 0 && rolled == 0)) {
+            basic.showNumber(8)
+            basic.showString("Today's Mission recommend this to your friends")
+            _8 = 1
+            rolled = 1
+        } else if (Mission == 9 && (_9 == 0 && rolled == 0)) {
+            basic.showNumber(9)
+            basic.showString("Today's Mission imitate your favorite videogame character (no swears)")
+            _9 = 1
+            rolled = 1
+        } else if (Mission == 10 && (_10 == 0 && rolled == 0)) {
+            basic.showNumber(10)
+            basic.showString("Today's Mission  imitate your least favorite videogame character")
+            _10 = 1
+            rolled = 1
+        } else if (Mission == 11 && (_11 == 0 && rolled == 0)) {
+            basic.showNumber(11)
+            basic.showString("Today's Mission If you play Roblox join your fav game and say \"I hate (insert game name)\"")
+            _11 = 1
+            rolled = 1
+        } else if (Mission == 12 && (_12 == 0 && rolled == 0)) {
+            basic.showNumber(12)
+            basic.showString("Today's Mission ask your friend if they ever experienced deja vu then ask them the same thing the next day ")
+            _12 = 1
+            rolled = 1
+        } else if (Mission == 13 && (_13 == 0 && rolled == 0)) {
+            basic.showNumber(13)
+            basic.showString("Today's Mission when your with your friend whisper \"I didn't say anything\" and when they ask what you said say \"I didn't say anything\"")
+            _13 = 1
+            rolled = 1
+        } else if (Mission == 14 && (_14 == 0 && rolled == 0)) {
+            basic.showNumber(14)
+            basic.showString("Today's Mission Hold the door for someone who is far away and smile at them the whole time")
+            _14 = 1
+            rolled = 1
+        } else if (Mission == 15 && (_15 == 0 && rolled == 0)) {
+            basic.showNumber(15)
+            basic.showString("Today's Mission during a normal conversation, Casually tell someone to  \"Calm down\"")
+            _15 = 1
+            rolled = 1
+        } else if (Mission == 16 && (_16 == 0 && rolled == 0)) {
+            basic.showNumber(16)
+            basic.showString("Today's Mission make a naughty list and put only your name on the nice list and put it on the ground.")
+            _16 = 1
+            rolled = 1
+        } else if (Mission == 17 && (_17 == 0 && rolled == 0)) {
+            basic.showNumber(17)
+            basic.showString("Today's task assuming you got a particular wording convert it into it's synonym instead ")
+            _17 = 1
+            rolled = 1
+        } else {
+            basic.showString("you thought huh? Ok your spared for now at least :).")
+        }
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Resetting...")
+    rolled = 0
+    _0 = 0
+    _1 = 0
+    _2 = 0
+    _3 = 0
+    _4 = 0
+    _5 = 0
+    _6 = 0
+    _7 = 0
+    _8 = 0
+    _9 = 0
+    _10 = 0
+    _11 = 0
+    _12 = 0
+    _13 = 0
+    _14 = 0
+    _15 = 0
+    _16 = 0
+    _17 = 0
+    basic.pause(randint(250, 1000))
+    basic.showString("Done!")
+})
+let Mission = 0
+let rolled = 0
+let _17 = 0
+let _16 = 0
+let _15 = 0
+let _14 = 0
+let _13 = 0
+let _12 = 0
+let _11 = 0
+let _10 = 0
+let _9 = 0
+let _8 = 0
+let _7 = 0
+let _6 = 0
+let _5 = 0
+let _4 = 0
+let _3 = 0
+let _2 = 0
+let _1 = 0
+let _0 = 0
+basic.showString("Today's Mission inspo: Freakbait")
+rolled = 0
